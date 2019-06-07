407f6cb6679ee6fe9d6f0c745911dc740195e0a8
hailong <hailengc@126.com>
2019-06-07 14:50:03 +0800

    refactor

--- content/contentScript.js
+++ content/contentScript.js
@@ -77,7 +77,6 @@ function queryAndShow(queryString, targetClientRect) {
 
       const horizontalOffset = targetClientRect.left + extWidth - docWidth;
       if (horizontalOffset > 0) {
-        console.log(".... horizontalOffset " + horizontalOffset);
         extensionContent.style.left =
           targetClientRect.left - horizontalOffset + "px";
       } else {
@@ -87,7 +86,6 @@ function queryAndShow(queryString, targetClientRect) {
       const verticalOffset = targetClientRect.bottom + extHeight - windowHeight;
       if (verticalOffset > 0) {
         extensionContent.style.top = targetClientRect.top - extHeight + "px";
-        console.log(" ... vertial offset ..." + verticalOffset);
       } else {
         extensionContent.style.top = targetClientRect.bottom + "px";
       }
@@ -95,8 +93,91 @@ function queryAndShow(queryString, targetClientRect) {
   });
 }
 
+class HoveringTarget {
+  static createNullTarget() {
+    return new HoveringTarget("", null);
+  }
+
+  constructor(targetString, targetClientRect) {
+    this.targetString = targetString;
+    this.targetClientRect = targetClientRect;
+    this.createdTime = Date.now();
+  }
+
+  _isRectEqual(rect1, rect2) {
+    if (rect1 == null || rect2 == null) {
+      return false;
+    }
+
+    return (
+      rect1.left == rect2.left &&
+      rect1.right == rect2.right &&
+      rect1.top == rect2.top &&
+      rect1.bottom == rect2.bottom
+    );
+  }
+
+  equalTo(ht) {
+    return (
+      this.isValid &&
+      this.targetString === ht.targetString &&
+      this._isRectEqual(this.targetClientRect, ht.targetClientRect)
+    );
+  }
+
+  get isValid() {
+    return isStringValid(this.targetString) && this.targetClientRect !== null;
+  }
+
+  get livingTime() {
+    return Date.now() - this.createdTime;
+  }
+
+  toString() {
+    return `${this.targetString} time: ${this.createdTime}, rect: ${this
+      .targetClientRect && this.targetClientRect.toString()}`;
+  }
+}
+
+function getHoveringTarget() {
+  let range = null,
+    textNode = null;
+
+  if (document.caretPositionFromPoint) {
+    range = document.caretPositionFromPoint(event.clientX, event.clientY);
+    textNode = range.offsetNode;
+    offset = range.offset;
+  } else if (document.caretRangeFromPoint) {
+    range = document.caretRangeFromPoint(event.clientX, event.clientY);
+    if (range) {
+      textNode = range.startContainer;
+      offset = range.startOffset;
+    }
+  }
+
+  let hoveringTarget = HoveringTarget.createNullTarget();
+  if (textNode && textNode.nodeType == 3) {
+    const selection = window.getSelection();
+    selection.empty();
+    selection.addRange(range);
+    selection.modify("move", "backward", "word");
+    selection.modify("extend", "forward", "word");
+    const slectionRange = selection.getRangeAt(0);
+    const rect = slectionRange.getBoundingClientRect();
+
+    const qstr = selection.toString().trim();
+    if (qstr && inRect(rect, event.clientX, event.clientY)) {
+      hoveringTarget = new HoveringTarget(qstr, getSeletionCR(selection));
+    }
+    selection.empty();
+  }
+
+  return hoveringTarget;
+}
+
 let currentQueryString = null,
-  isSelecting = false;
+  isSelecting = false,
+  lastHoveringTarget = HoveringTarget.createNullTarget();
 
 document.addEventListener("mouseup", event => {
   console.log("... mouseup...");
@@ -106,7 +187,7 @@ document.addEventListener("mouseup", event => {
     const selectedString = sel.toString().trim();
 
     if (selectedString) {
-      queryAndShow(selectedString, getSeletionCR(sel));
+      // queryAndShow(selectedString, getSeletionCR(sel));
     } else {
       hideExtensionContainer();
     }
@@ -124,47 +205,35 @@ document.addEventListener("selectionchange", event => {});
 
 document.addEventListener("mousemove", event => {
   if (isSelecting || hasValidSelection()) {
-    console.log("... sking hovering query...");
+    console.log("... sking hovering  for existing selection...");
     return;
   }
 
-  console.log("... mouse moving ...");
+  const hoveringTarget = getHoveringTarget();
 
-  let range = null,
-    textNode = null;
-
-  if (document.caretPositionFromPoint) {
-    range = document.caretPositionFromPoint(event.clientX, event.clientY);
-    textNode = range.offsetNode;
-    offset = range.offset;
-  } else if (document.caretRangeFromPoint) {
-    range = document.caretRangeFromPoint(event.clientX, event.clientY);
-    if (range) {
-      textNode = range.startContainer;
-      offset = range.startOffset;
-    }
+  if (!hoveringTarget.equalTo(lastHoveringTarget)) {
+    // update hovering target
+    lastHoveringTarget = hoveringTarget;
   }
 
-  if (textNode && textNode.nodeType == 3) {
-    const selection = window.getSelection();
-    selection.empty();
-    selection.addRange(range);
-    selection.modify("move", "backward", "word");
-    selection.modify("extend", "forward", "word");
-    const slectionRange = selection.getRangeAt(0);
-    const rect = slectionRange.getBoundingClientRect();
-
-    if (inRect(rect, event.clientX, event.clientY)) {
-      const qstr = selection.toString().trim();
-      if (qstr) {
-        queryAndShow(qstr, getSeletionCR(selection));
-      } else {
-        hideExtensionContainer();
-      }
-    } else {
-      hideExtensionContainer();
-    }
-
-    selection.empty();
-  }
+  // if (lastHoveringTarget !== null) {
+  //   // queryAndShow(
+  //   //   lastHoveringTarget.targetString,
+  //   //   lastHoveringTarget.targetClientRect
+  //   // );
+  // } else {
+  //   // hideExtensionContainer();
+  // }
 });
+
+const CHECK_INTERVAL = 20;
+const VALID_HOVERING_TIME = 300;
+
+setInterval(() => {
+  if (
+    lastHoveringTarget.isValid &&
+    lastHoveringTarget.livingTime >= VALID_HOVERING_TIME
+  ) {
+    console.log(lastHoveringTarget.toString());
+  }
+}, CHECK_INTERVAL);
